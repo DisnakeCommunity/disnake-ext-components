3d3f89dd7635b7d4101e63b0412b53f21ae69732
Chromosomologist <55550164+Chromosomologist@users.noreply.github.com>
2022-07-22 20:42:43 +0200

    meta: bump version (0.3.3 -> 0.4.0)

--- disnake_ext_components/__init__.py
+++ disnake_ext_components/__init__.py
@@ -7,7 +7,7 @@ listeners somewhat less cumbersome.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 
 from . import patterns, utils  # pyright: ignore  # TODO: explicit imports and __all__
