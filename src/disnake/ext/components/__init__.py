3b1ac771b89c3b35a1cc04a2f4b3c5c7cfc4826b
Chromosomologist <chromosomologist@gmail.com>
2023-03-16 01:58:44 +0100

    feat: fill out top-level __init__.py

--- src/disnake/ext/components/__init__.py
+++ src/disnake/ext/components/__init__.py
@@ -1,3 +1,7 @@
+# pyright: reportImportCycles = false
+# pyright: reportWildcardImportFromLibrary = false
+# ^ This is a false positive as it is confused with site-packages' disnake.
+
 """The main disnake-ext-components module.
 
 An extension for disnake aimed at making component interactions with
@@ -5,3 +9,7 @@ listeners somewhat less cumbersome.
 """
 
 from disnake.ext.components import api as api
+from disnake.ext.components import internal as internal
+from disnake.ext.components.fields import *
+from disnake.ext.components.impl import *
+from disnake.ext.components.interaction import *
