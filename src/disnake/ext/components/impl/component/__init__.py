aa33f3c8bb2f336790613e8a61c32dc1ffe4f483
Chromosomologist <chromosomologist@gmail.com>
2023-03-16 22:47:46 +0100

    feat: export component classes

---
+++ src/disnake/ext/components/impl/component/__init__.py
@@ -0,0 +1,8 @@
+# pyright: reportImportCycles = false
+# pyright: reportWildcardImportFromLibrary = false
+# ^ This is a false positive as it is confused with site-packages' disnake.
+
+"""Implementations for all kinds of component classes."""
+
+from disnake.ext.components.impl.component.base import *
+from disnake.ext.components.impl.component.button import *
