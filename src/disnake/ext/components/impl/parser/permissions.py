d910589edbdb033aea41042764e9b5071beb5e5d
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2023-03-21 22:46:36 +0100

    implement other parser types

---
+++ src/disnake/ext/components/impl/parser/permissions.py
@@ -0,0 +1,19 @@
+"""Parser implementations for disnake permissions types."""
+
+from __future__ import annotations
+
+import typing
+
+import disnake
+from disnake.ext.components.impl.parser import base, snowflake
+
+__all__: typing.Sequence[str] = (
+    "PermissionsParser",
+)
+
+
+PermissionsParser = base.Parser.from_funcs(
+    lambda _, argument: disnake.Permissions(int(argument)),
+    lambda argument: str(argument.value),
+    is_default_for=(disnake.Permissions,)
+)
