58fbf4aeeb6bc959c233e9aa2d64d3d8fe5325b7
Chromosomologist <chromosomologist@gmail.com>
2023-05-11 14:58:18 +0200

    chore: delete remaining custom id code

--- src/disnake/ext/components/impl/__init__.py
+++ src/disnake/ext/components/impl/__init__.py
@@ -5,7 +5,6 @@
 """Default concrete implementations for types in ``disnake.ext.components.api``."""
 
 from disnake.ext.components.impl.component import *
-from disnake.ext.components.impl.custom_id import *
 from disnake.ext.components.impl.factory import *
 from disnake.ext.components.impl.manager import *
 from disnake.ext.components.impl.parser import *
