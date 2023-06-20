1b6b92b87100fec5bcd6a5c75ed62e96ba172cb4
Chromosomologist <chromosomologist@gmail.com>
2023-06-21 00:26:38 +0200

    refactor!: change how parsers are exported

--- src/disnake/ext/components/impl/parser/__init__.py
+++ src/disnake/ext/components/impl/parser/__init__.py
@@ -6,5 +6,12 @@
 
 from disnake.ext.components.impl.parser.base import *
 from disnake.ext.components.impl.parser.channel import *
+from disnake.ext.components.impl.parser.emoji import *
+from disnake.ext.components.impl.parser.guild import *
+from disnake.ext.components.impl.parser.invite import *
+from disnake.ext.components.impl.parser.message import *
+from disnake.ext.components.impl.parser.permissions import *
+from disnake.ext.components.impl.parser.role import *
 from disnake.ext.components.impl.parser.snowflake import *
 from disnake.ext.components.impl.parser.stdlib import *
+from disnake.ext.components.impl.parser.user import *
